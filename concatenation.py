bf825f92680f44009a221e13a04ff046f5147bc4
Chris Nd <chrisndouassi@gmail.com>
2026-02-22 00:03:04 +0100

    concaténation

---
+++ concatenation.py
@@ -0,0 +1,11 @@
+# Concatenation de chaînes et conversion de types
+
+a = "J'ai une classe de " + str(30) + " élèves."
+b = "10 + 5 est égale à " + str(15) + "."
+c = 10 + int("5")
+d = "Le résultat de 10 + 5 est " + str(10 + 5) + "."
+
+print(a)
+print(b)
+print(c)
+print(d)
