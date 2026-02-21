d693306b01a0746f98d2d00da17e6633dd93e40b
Chris Nd <chrisndouassi@gmail.com>
2026-02-21 23:54:27 +0100

    Les opérateurs de comparaison

---
+++ comparison_operator.py
@@ -0,0 +1,28 @@
+# Opérateurs de comparaison en Python
+
+i = 5
+j = 10
+
+print(i == j)  # Égal à (False)
+print(i != j)  # Différent de (True)
+print(i < j)   # Inférieur à (True)
+print(i <= j)  # Inférieur ou égal à (True)
+print(i > j)   # Supérieur à (False)
+print(i >= j)  # Supérieur ou égal à (False)
+
+# Comparaison d'objets
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(a == b)  # Égal à (True)
+print(a is b)  # Identique (False)
+c = a
+print(a is c)  # Identique (True)
+
+# Comparaison de valeurs entre -5 et 256
+
+a = 10
+b = 10
+print(a == b)  # Égal à (True)
+print(a is b)  # Identique (True)
+
