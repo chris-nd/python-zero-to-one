52e13eb5ff7563e5c58f22ad84b7f2ed07d45d4d
Chris Nd <chrisndouassi@gmail.com>
2026-02-21 23:42:13 +0100

    les différents types de données

---
+++ data_type.py
@@ -0,0 +1,17 @@
+print("Hello, World!") # Type String
+print("""
+      This is a multi-line string.
+      It spans several lines.
+      """) # Type String
+print(42) #Type Integer
+print(3.14) # Type Float
+print(True) # Type Boolean
+print(False) # Type Boolean
+print(None) # Type NoneType
+print([1, 2, 3]) # Type List
+print((4, 5, 6)) # Type Tuple
+print({ 'a': 1, 'b': 2 }) # Type Dictionary
+print({7, 8, 9}) # Type Set
+print({1, 2, 3}) # Type Frozenset
+print(range(5)) # Type Range
+print(2 + 3j) # Type Complex
