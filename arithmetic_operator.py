88f0d676d354831e922280dacdddfa60841beb0f
Chris Nd <chrisndouassi@gmail.com>
2026-02-21 23:53:16 +0100

    les opérateurs arithmétiques

---
+++ arithmetic_operator.py
@@ -0,0 +1,29 @@
+# Arithmetic Operators in Python
+
+print(3 + 5)  # Addition (8)
+print(10 - 4)  # Soustraction et division (6)
+print(10 * 2)  # Multiplication (20)
+print(8 / 4)  # Division (2.0)
+print(7 % 3)  # Modulo (1)
+print(2 ** 3)  # Exponentiation (8)
+print(10 // 3)  # Division entière (3)
+
+# Variable assignment and operations
+
+i = 1
+i = i + 1 
+print(i)  # Addition (2)
+i += 2
+print(i)  # Addition (4)
+i -= 1
+print(i)  # Soustraction (3)
+i *= 3
+print(i)  # Multiplication (9)
+i /= 3
+print(i)  # Division (3.0)
+i %= 2
+print(i)  # Modulo (1.0)
+i **= 4
+print(i)  # Exponentiation (1.0)
+i //= 1
+print(i)  # Division entière (1.0)
