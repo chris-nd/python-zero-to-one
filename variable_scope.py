1be3a22cc802875b54dd4883fb9344b9192edd0a
Chris Nd <chrisndouassi@gmail.com>
2026-03-11 11:42:45 +0100

    gestion de l’exception UnboundLocalError

--- variable_scope.py
+++ variable_scope.py
@@ -97,3 +97,65 @@ try:
     print(j)
 except NameError as e:
     print('OOPS', e)
+
+# L’exception UnboundLocalError
+
+def ma_fonction1():
+    variable1 = "locale"
+    print(variable1)
+
+ma_fonction1()
+
+variable2 = "globale"
+
+def ma_fonction2():
+    variable2 = "locale"
+    print(variable2)
+
+ma_fonction2()
+
+# Variable globale
+# On peut accéder en lecture à une variable globale sans précaution particulière
+variable3 = "globale"
+
+def ma_fonction3():
+    print(variable3)
+
+ma_fonction3()
+
+# cet exemple ne fonctionne pas et lève UnboundLocalError
+variable4 = "globale"
+
+def ma_fonction4():
+    # on référence la variable globale
+    print(variable4)
+    # et maintenant on crée une variable locale
+    variable4 = "locale"
+
+# on "attrape" l'exception
+try:
+    ma_fonction4()
+except Exception as e:
+    print(f"OOPS, exception {type(e)}:\n{e}")
+
+# Une variable dans une fonction peut être ou bien locale 
+# si elle est affectée dans la fonction ou bien globale 
+# mais pas les deux à la fois
+
+# Si vous avez une erreur UnboundLocalError, 
+# c’est qu’à un moment donné il a eu cette confusion
+
+# Pour résoudre ce conflit il faut explicitement
+# déclarer la variable  comme globale
+variable5 = "globale"
+
+def ma_fonction5():
+    global variable5
+    # on référence la variable globale
+    print("dans la fonction", variable5)
+    # cette fois on modifie la variable globale
+    variable5 = "changée localement"
+
+ma_fonction5()
+print("après la fonction", variable5)
+
