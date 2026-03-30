d1b68f8bdeea3248b9969063343a42b0506b1029
Chris Nd <chrisndouassi@gmail.com>
2026-03-30 16:16:40 +0200

    Compréhension de set et de dictionnaire

---
+++ dict_comprehension.py
@@ -0,0 +1,2 @@
+dictionnaire = d = {x: ord(x) for x in 'abc'}
+print(dictionnaire) # {'a': 97, 'b': 98, 'c': 99}
---
+++ set_comprehension.py
@@ -0,0 +1,2 @@
+ensenmbe = {x**2 for x in (1, 2, -1, 2)}
+print(ensenmbe) # {1, 4} (Les doublons sont supprimés)
