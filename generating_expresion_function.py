df8daf62fd9c55868fc30186c715de5bbaa0bc59
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-05-25 23:50:16 +0200

    Refactoring + fonction génératrice avec générateur chaîné

--- generating_expresion_function.py
+++ generating_expresion_function.py
@@ -9,7 +9,11 @@ print(carre) # <generator object <genexpr> at 0x...>
 
 print(sum(carre)) # 333333000
 print(sum(carre)) # 0 parce que le générateur, qui est un itérateur, est épuisé
-print(next(carre)) # Lève une exception StopIteration
+
+try:
+    print(next(carre)) # Lève une exception StopIteration
+except StopIteration as e:
+    print("Le générateur est épuisé", e)
 
 # Générateur de nombres palindromes
 gen_carre = (x**2 for x in range(1_000))
@@ -26,33 +30,68 @@ print(gen()) # <generator object gen at 0x...>
 
 g = gen() 
 print(next(g)) # 10
-print(next(g)) # Lève une exception StopIteration
 
-def gen(x): 
+try:
+    print(next(g)) # Lève une exception StopIteration
+except StopIteration as e:
+    print("Le générateur est épuisé", e)
+
+def generator(x): 
     yield x 
     x = x + 1 
     yield x
 
-g = gen(15) 
+g = generator(15) 
 print(next(g)) # 15
 print(next(g)) # 16
-print(next(g)) # Lève une exception StopIteration
+
+try:
+    print(next(g)) # Lève une exception StopIteration
+except StopIteration as e:
+    print("Le générateur est épuisé", e)
 
 # Fonction génératrice pour les carrés
-def carre(a, b): 
-    for i in range(a, b): 
+def square(a, b):
+    for i in range(a, b):
         yield i ** 2
 
-c = carre(1, 10) # Création d'un générateur
+c = square(1, 10) # Création d'un générateur
 print(list(c)) # [1, 4, 9, 16, 25, 36, 49, 64, 81]
 
 # Fonction génératrice pour les nombres palindromes
-def palin(it): 
+def palindrome(it): 
     for i in it: 
         if (isinstance(i, (str, int)) and
             str(i) == str(i)[::-1]): 
             yield i
 
-p = palin([121, 10, 12321, 'abc', 'abba']) # Création d'un générateur
+p = palindrome([121, 10, 12321, 'abc', 'abba']) # Création d'un générateur
 print(list(p)) # [121, 12321, 'abba']
-print(list(palin(x**2 for x in range(1000)))) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print(list(palindrome(x**2 for x in range(1000)))) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+# Générateur de diviseurs
+
+def divs(n):
+    for i in range(2, n):
+        if n % i == 0: yield i # 
+
+# Générateur de diviseurs de diviseurs
+
+def divdivs_ko(n):
+    for i in divs(n):
+        yield divs(i)
+
+print(divs(12)) # <generator object <genexpr> at 0x...>
+print(list(divs(12))) # [2, 3, 4, 6]
+print(divdivs_ko(12)) # <generator object <genexpr> at 0x...>
+print(list(divdivs_ko(12))) # [<generator object divs at 0x10bf32500>, <generator object divs at 0x10bf320a0>, <generator object divs at 0x10bf32340>, <generator object divs at 0x10bf32420>]
+
+def divdivs_ok_2(n):
+    for i in divs(n):
+        # ✅ "Extraie chaque valeur de divs(i) et yield-les une par une"
+        yield from divs(i)
+
+print(divdivs_ok_2(12))
+print(list(divdivs_ok_2(12)))
+
+round
