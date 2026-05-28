3a4478022eccb91aaca1dce1f2ec7d2b55746750
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-05-28 18:13:20 +0200

    Surcharge de méthode dans une classe enfant

--- inheritance.py
+++ inheritance.py
@@ -16,3 +16,43 @@ print("l'objet baleine est-il un mammifère ?", isinstance(baleine, Mammifere))
 
 # ici c'est moins évident, mais la réponse est 'True' aussi
 print("l'objet baleine est-il un animal ?", isinstance(baleine, Animal))
+
+# Méthodes spéciales (dunders)
+
+class Phrase:
+    def __init__(self, ma_phrase):
+        self.ma_phrase = ma_phrase
+        self.mots = ma_phrase.split()
+
+    def nb_lettres(self):
+        return len(self.ma_phrase)
+
+    def __len__(self):
+        return len(self.mots)
+
+    def __contains__(self, mot):
+        return mot in self.mots
+
+    def __str__(self):
+        return self.ma_phrase
+
+
+p = Phrase("Bonjour, le monde!")
+print(len(p)) # 3
+print("monde" in p) # True
+print(p) # Bonjour, le monde!
+
+
+class PhraseSansCasse(Phrase):
+    def __init__(self, ma_phrase):
+        self.mots_lower = {mot.lower() for mot in ma_phrase.split()}
+        Phrase.__init__(self, ma_phrase)
+
+    def __contains__(self, mot):
+        return mot.lower() in self.mots_lower
+
+
+p2 = PhraseSansCasse("Bonjour, le monde!")
+print(p2) # Bonjour, le monde!
+print("monde" in p2) # True
+print("BONJOUR" in p2) # True
