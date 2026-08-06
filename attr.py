b8f5aca4a062418c432dd38e06aa7304bbee67ae
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-08-06 11:07:54 +0200

    Update

--- attr.py
+++ attr.py
@@ -1,9 +1,16 @@
-# getattr()
+"getattr, setattr, hasattr, delattr"
 
-class Person1: 
-    def __init__(self, name, age): 
-        self.name = name 
-        self.age = age 
+# La résolution des attributes s'éffectue dynamiquement au moment de l'exécution(runtime)
+# alors que la liaison des variables s'effectue lexicalement et statiquement au moment de
+# la compilation(compile time).
+
+# getattr()
+# permet de récupérer la valeur d'un attribut
+class Person1:
+    "Classe objet de type Person1"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
 person = Person1('John Doe', 30)
 
@@ -11,10 +18,11 @@ attr_name = input('Enter the attribute you want to see: ')
 print(getattr(person, attr_name, 'Attribute not found'))
 
 
-class Person2: 
-    def __init__(self, name, age): 
-        self.name = name 
-        self.age = age 
+class Person2:
+    "Classe objet de type Person2"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
 person = Person2('John Doe', 30)
 
@@ -27,7 +35,18 @@ for attr in dir(person):
 
 
 # setattr()
+# permet de définir la valeur d'un attribut
+def func():
+    "documentation de la fonction foo"
+    pass
+
+setattr(func, "width", 100)
+
+print("width:", func.width)
+
+
 class Configuration:
+    "Classe objet de type Configuration"
     pass
 
 # Données chargées à l'exécution
@@ -50,7 +69,7 @@ print(config_obj.max_retries) # 5
 
 
 # hasattr()
-
+# permet de vérifier si un attribut existe dans l'espase de nommage de l'objet
 class Product:
     def __init__(self, name, price):
         self.name = name
@@ -67,9 +86,21 @@ for attr in required_attributes:
         # Accès aux attributs dynamiquement une fois leur existence confirmée
         print(f'{attr}: {getattr(product_a, attr)}')
 
+# Il est impossible de rajouter des attributs sur les
+# types de base, car ce sont des classes immuables.
+# C'est par contre possible sur virtuellement tout le reste:
+# modules, packages, fonctions, classes, instances
+
+for builtin_type in (int, str, float, complex, tuple, dict, set, frozenset):
+    obj = builtin_type()
+    try:
+        obj.foo = 'bar'
+    except AttributeError as e: 
+        print(f"{builtin_type.__name__:>10} → exception {type(e)} - {e}")
+
 
 # delattr()
-
+# permet de supprimer un attribut de l'espace de nommage d'un objet
 class UserSession2:
     def __init__(self, user_id, token):
         self.user_id = user_id
