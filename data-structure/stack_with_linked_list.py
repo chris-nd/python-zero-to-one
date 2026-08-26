96071da31c6616ac473e886a1b1a8a2144e26373
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-08-26 23:50:36 +0200

    Docs: update

--- data-structure/stack_with_linked_list.py
+++ data-structure/stack_with_linked_list.py
@@ -67,9 +67,11 @@ class Stack:
             Le nombre d'éléments dans la pile.
         """
         return self.stack_size
-<<<<<<< HEAD
 
     def traverse(self):
+        """
+        Parcourt la liste chaînée et affiche les valeurs.
+        """
         current_node = self.head
         while current_node:
             print(current_node.value, end=" -> ")
@@ -89,5 +91,3 @@ print("Liste chaînée après Pop: ", end="")
 my_stack.traverse()
 print("isEmpty: ", my_stack.isEmpty())
 print("Size: ", my_stack.size())
-=======
->>>>>>> refs/remotes/origin/main
