00ef3071fc33b2ea6e316f1086d2e5f6d424d7e1
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-08-26 14:27:34 +0200

    rechercher la valeur minimal

--- data-structure/singly_linked_list.py
+++ data-structure/singly_linked_list.py
@@ -18,6 +18,17 @@ class Node:
         self.data = data
         self.next = None
 
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
 def traverse(head: Node) -> None:
     """
     Parcourir une liste simplement chaînée
@@ -31,15 +42,25 @@ def traverse(head: Node) -> None:
         current_node = current_node.next
     print("null")
 
-node1 = Node(7)
-node2 = Node(11)
-node3 = Node(3)
-node4 = Node(2)
-node5 = Node(9)
-
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node5
-
 traverse(node1)
+
+
+# Trouver la valeur minimal dans une liste chaînée
+def find_min_value(head: Node) -> Any:
+    """
+    Trouver la valeur minimal d'une liste chaînée
+
+    Args:
+        head (Node): Le noeud de tête
+    """
+
+    min_value = head.data
+    current_node = head.next
+
+    while current_node:
+        if current_node.data < min_value:
+            min_value = current_node.data
+        current_node = current_node.next
+    return min_value
+
+print("La valeur minimale dans la liste chaînée est:", find_min_value(node1))
