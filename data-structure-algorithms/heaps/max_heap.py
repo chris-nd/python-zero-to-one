c640d7a5272f8f5fad2f9fa70b5a13f3bda2b203
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-08-28 23:47:49 +0200

    Docs: Définition d'un tas minimal et maximal

---
+++ data-structure-algorithms/heaps/max_heap.py
@@ -0,0 +1,6 @@
+"""
+Structure de données : Tas maximal (Max Heap)
+
+C'est une file de priorité implémentée sous forme d'arbre binaire où l'élément 
+prioritaire est à toujours la plus grande valeur (la racine ou au parent).
+"""
---
+++ data-structure-algorithms/heaps/min_heap.py
@@ -0,0 +1,6 @@
+"""
+Structure de données : Tas minimal (Min Heap)
+
+C'est une file de priorité implémentée sous forme d'arbre binaire où l'élément 
+prioritaire est à toujours la plus petite valeur (la racine ou au parent).
+"""
